2a9ce3fb4af989556444ee87f191a1bce92db60b
itggot-jonas-rost <jonas.rost@itggot.se>
2017-04-25 15:16:57 +0200

    Profil-sidan

---
+++ site/js/script.js
@@ -0,0 +1,15 @@
+function Brag_dropdown() {
+    document.getElementById("brag_Dropdown").classList.toggle("show");
+}
+
+function Customize_dropdown() {
+    document.getElementById("customize_Dropdown").classList.toggle("show");
+}
+
+function change_profile_pic() {
+    document.getElementById("profile_pic1").classList.toggle("clicked1");
+}
+
+function change_style() {
+    document.getElementById("profile_site").classList.toggle("clicked2");
+}
