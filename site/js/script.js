6137701a8e5f0cf6df715c466559083e4e7dbec2
itggot-jonas-rost <jonas.rost@itggot.se>
2017-05-09 15:15:53 +0200

    Jobbat på social samt lagt till settings slide

--- site/js/script.js
+++ site/js/script.js
@@ -1,6 +1,6 @@
 function toggle_site() {
     document.getElementById("profile_site").classList.toggle("hide");
-    document.getElementById("social_site").classList.toggle("show");
+    document.getElementById("social_site").classList.toggle("social_site_show");
     document.getElementById("toggle_site").classList.toggle("clicked3");
     
 }
@@ -24,17 +24,25 @@ function change_style() {
 var slideIndex = 1;
 change_slide_img(slideIndex);
 
-function change_slideshow_show(n) {
+function change_slideshow_profile_show(n) {
     change_slide_img(slideIndex += n);
 }
 
 function change_slide_img(n) {
     var i;
-    var x = document.getElementsByClassName("slideshow_img");
+    var x = document.getElementsByClassName("slideshow_img_profile");
     if (n > x.length) {slideIndex = 1} 
     if (n < 1) {slideIndex = x.length} ;
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none"; 
     }
     x[slideIndex-1].style.display = "block"; 
+}
+
+function openSettings() {
+    document.getElementById("mySettings").style.width = "67%";
+}
+
+function closeSettings() {
+    document.getElementById("mySettings").style.width = "0";
 }
